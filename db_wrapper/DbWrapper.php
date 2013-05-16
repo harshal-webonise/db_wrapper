df746685872a99586121186c94ac4161288c1d27
Sushant Gawali <sushant@weboniselab.com>
2013-05-16 12:17:29 +0530

    working on select

--- db_wrapper/DbWrapper.php
+++ db_wrapper/DbWrapper.php
@@ -1,17 +1,32 @@
 <?php
 class DbWrapper extends PDO
 {
-    private static $db;
+    private static $db, $query;
 
     function __construct($hostName, $userName, $password, $databaseName)
     {
-        return parent::__construct("mysql:host=$hostName;dbname=$databaseName", $userName, $password);
+        try {
+
+            parent::__construct("mysql:host=$hostName;dbname=$databaseName", $userName, $password);
+
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
     }
 
     public static function getInstance($hostName, $userName, $password, $databaseName)
     {
         if (self::$db === null) {
             self::$db = new DbWrapper($hostName, $userName, $password, $databaseName);
+        }
+        return self::$db;
+    }
+
+    public function select($fields)
+    {
+        self::$query = 'select ';
+        foreach ($fields as $field) {
+            self::$query += $field . ', ';
         }
         return self::$db;
     }
--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
 
+require_once('db_wrapper/DbWrapper.php');
+
+$conn = DbWrapper::getInstance("localhost","root","webonise6186","db_wrapper");
+
+
